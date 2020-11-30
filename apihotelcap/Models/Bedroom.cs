10df7e40b7b8e9eb5b927650aae474482c2cc5c8
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-30 15:55:28 -0300

    Modificando model de Bedroom

---
+++ apihotelcap/Models/Bedroom.cs
@@ -0,0 +1,27 @@
+﻿using apihotelcap.Enums;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace apihotelcap.Models.RequestModels
+{
+    public class Bedroom
+    {
+        [Description("Andar")]
+        public int Floor { get; set; }
+
+        [Description("Número do Quarto")]
+        public int NoBedroom { get; set; }
+
+        public string Situation { get; set; }
+
+        public int IdBedroomType { get; set; }
+
+        public Bedroom() { }
+    }
+}

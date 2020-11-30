c59c699daa0575c929b39c242123d51e73d88e5b
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-30 15:57:14 -0300

    Adicionando Implementações de bedroom nas camadas service e repository

---
+++ apihotelcap/Repository/BedroomRepository.cs
@@ -0,0 +1,58 @@
+﻿using apihotelcap.Interfaces.Repository;
+using apihotelcap.Models.RequestModels;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apihotelcap.Repository
+{
+    public class BedroomRepository : IBedroomRepository
+    {
+        private readonly string _connectionString;
+
+        public BedroomRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public Bedroom InsertBedroom(Bedroom bedroom)
+        {
+            var connection = new SqlConnection(_connectionString);
+
+            var query = "insert into BedRoom (Floor, NoBedroom, Situation, IdBedroomType) " +
+                            "values (@Floor, @NoBedroom, @Situation, @IdBedroomType)";
+
+            var result = connection.Execute(query, new
+            {
+                Floor = bedroom.Floor,
+                NoBedroom = bedroom.NoBedroom,
+                Situation = bedroom.Situation,
+                IdBedroomType = bedroom.IdBedroomType
+            });
+
+            Debug.WriteLine("Quarto cadastrado com sucesso");
+            return bedroom;
+        }
+
+        /// <summary>
+        /// Metodo que verifica se o tipo de quarto existe no banco
+        /// </summary>
+        /// <param name="IdBedroomType"></param>
+        public int VerifyIfBedroomTypeExists(int IdBedroomType)
+        {
+            var connection = new SqlConnection(_connectionString);
+
+            var query = "select count(IdBedroomType) from BedroomType " +
+                            " where Id = @IdBedroomType";
+
+            var result = connection.Query<int>(query, new { IdBedroomType = IdBedroomType });
+
+            return result.FirstOrDefault();
+        }
+    }
+}
---
+++ apihotelcap/Services/BedroomService.cs
@@ -0,0 +1,54 @@
+﻿using apihotelcap.Enums;
+using apihotelcap.Interfaces.Repository;
+using apihotelcap.Interfaces.Services;
+using apihotelcap.Models.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using static apihotelcap.Enums.BedroomType;
+
+namespace apihotelcap.Services
+{
+    public class BedroomService : IBedroomService
+    {
+        private readonly IBedroomRepository _repo;
+
+        public BedroomService(IBedroomRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public Bedroom InsertBedroom(Bedroom bedroom)
+        {
+            var result = ValidateBedroomChilds(bedroom);
+
+            if (result)
+            {
+                int IdBedroomType = _repo.VerifyIfBedroomTypeExists(bedroom.IdBedroomType);
+                if (IdBedroomType != 0)
+                {
+                    _repo.InsertBedroom(bedroom);
+                    return bedroom;
+                }
+                else
+                    throw new Exception($"O quarto com ID {bedroom.IdBedroomType} não está cadastrado");
+            }
+            else
+                return new Bedroom();
+        }
+
+        public bool ValidateBedroomChilds(Bedroom bedroom)
+        {     
+            if (bedroom.NoBedroom < 1)
+                throw new Exception("O número de quartos não pode ser menor que 1");
+            else if (!bedroom.Situation.Contains(nameof(BedroomTypeValues.A)) || !bedroom.Situation.Contains(nameof(BedroomTypeValues.I)))
+                throw new Exception($"A situação do quarto não pode ser {bedroom.Situation}, são aceitos apenas A (Ativo) e I (Inativo)");
+            else if (bedroom.IdBedroomType == 0)
+                throw new Exception("O Id do quarto não pode ser 0");
+            else
+                return true;
+        }
+    }
+}

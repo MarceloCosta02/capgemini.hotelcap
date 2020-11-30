d925bbe59f6933db1f4d92e32384ddbe9c1d7441
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-30 15:56:46 -0300

    Adicionando Enums de BedroomType

---
+++ apihotelcap/Enums/BedroomType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apihotelcap.Enums
+{
+    public class BedroomType
+    {
+        public enum BedroomTypeValues
+        {
+            /// <summary>
+            /// Active
+            /// </summary>
+            [Description("Active")]
+            A,
+
+            /// <summary>
+            /// Inactive
+            /// </summary>
+            [Description("Inactive")]
+            I
+        }
+    }
+}

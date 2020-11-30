dd005e35029a905da259c64248a4ec036f5753a4
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-30 12:14:05 -0300

    Adicionando e configurando o Swagger e o NewtonsoftJson

---
+++ apihotelcap/Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace apihotelcap.Configuration
+{
+    public static class SwaggerConfiguration
+    {
+        public static IServiceCollection ConfigurarSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(swagger =>
+            {
+                swagger.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "Hotel V1",
+                    Description = "Hotel V1"
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                swagger.IncludeXmlComments(xmlPath);
+
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UtilizarConfiguracaoSwagger(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Hotel API V1");
+                c.DocumentTitle = "Hotel API";
+                c.DocExpansion(DocExpansion.None);
+            });
+            return app;
+        }
+    }
+}
--- apihotelcap/Startup.cs
+++ apihotelcap/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using apihotelcap.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,7 +26,8 @@ namespace apihotelcap
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(); 
+            services.ConfigurarSwagger();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +41,8 @@ namespace apihotelcap
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UtilizarConfiguracaoSwagger();
 
             app.UseAuthorization();
 

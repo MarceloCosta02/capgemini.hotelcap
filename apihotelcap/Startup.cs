632b5c42e6877ed14e5de69390fa3eca5fe5b29b
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-30 15:57:28 -0300

    Adicionando injeção de dependencia

--- apihotelcap/Startup.cs
+++ apihotelcap/Startup.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using apihotelcap.Configuration;
+using apihotelcap.Interfaces.Repository;
+using apihotelcap.Interfaces.Services;
+using apihotelcap.Repository;
+using apihotelcap.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +32,9 @@ namespace apihotelcap
         {
             services.AddControllers().AddNewtonsoftJson(); 
             services.ConfigurarSwagger();
+
+            services.AddScoped<IBedroomService, BedroomService>();
+            services.AddScoped<IBedroomRepository, BedroomRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

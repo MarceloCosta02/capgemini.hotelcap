3a38eca9c8f4373f617a1729bcc9c72cabede764
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-11-30 15:55:52 -0300

    Criando interfaces de repository e service de Bedroon

---
+++ apihotelcap/Interfaces/Repository/IBedroomRepository.cs
@@ -0,0 +1,23 @@
+﻿using apihotelcap.Models.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apihotelcap.Interfaces.Repository
+{
+    public interface IBedroomRepository
+    {
+        /// <summary>
+        /// Metodo que cadastra os quartos no banco
+        /// </summary>
+        /// <param name="bedroom"></param>
+        Bedroom InsertBedroom(Bedroom bedroom);
+
+        /// <summary>
+        /// Metodo que verifica se o tipo de quarto existe no banco
+        /// </summary>
+        /// <param name="IdBedroomType"></param>
+        int VerifyIfBedroomTypeExists(int IdBedroomType);
+    }
+}
---
+++ apihotelcap/Interfaces/Services/IBedroomService.cs
@@ -0,0 +1,17 @@
+﻿using apihotelcap.Models.RequestModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apihotelcap.Interfaces.Services
+{
+    public interface IBedroomService
+    {
+        /// <summary>
+        /// Metodo que insere quartos
+        /// </summary>
+        /// <param name="bedroom"></param>
+        Bedroom InsertBedroom(Bedroom bedroom);
+    }
+}

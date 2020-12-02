f6dbc31e64ff656aa40650cca5a7f2a428176e2b
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-12-01 23:23:39 -0300

    Criando testes integrados BDD de Client

---
+++ apihotelcap.IntegratedTest/Client.feature
@@ -0,0 +1,19 @@
+﻿Funcionalidade: Client
+Testes integrados das funcionalidade relacionadas ao end-point Client
+
+
+Esquema do Cenario: Listar Clientes
+	Dado que o endpoint é 'Client'
+	E que o método http é 'GET'
+	E que o id é 1
+	Quando obter o cliente
+	Então a resposta será 200	
+
+Esquema do Cenario: Inserir Clientes
+	Dado que o endpoint é 'Client'
+	E que o método http é 'POST'
+	E que o name é "Marcelo"
+	E que o cpf é "44495985809"
+	E que o hash é "GDUS11HDU"
+	Quando obter o cliente
+	Então a resposta será 201	
---
+++ apihotelcap.IntegratedTest/Steps/ClientSteps.cs
@@ -0,0 +1,95 @@
+﻿using BoDi;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace apihotelcap.IntegratedTest.Steps
+{
+    [Binding]
+    public class ClientStep
+    {
+        private string _host = "https://localhost:44314/api/";
+        private IRestClient _restClient;
+        private IRestRequest _restRequest;
+        private IRestResponse _restResponse;
+        private IObjectContainer _objectContainer;
+        private int _id;
+        private string _name;
+        private string _cpf;
+        private string _hash;
+
+        public ClientStep(IObjectContainer objectContainer) => _objectContainer = objectContainer;
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            _restClient = new RestClient();
+            _objectContainer.RegisterInstanceAs(_restClient);
+            _restRequest = new RestRequest();
+            _objectContainer.RegisterInstanceAs(_restRequest);
+            _restResponse = new RestResponse();
+            _objectContainer.RegisterInstanceAs(_restResponse);
+            _restClient.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+        }        
+
+        [Given(@"que o endpoint é '(.*)'")]
+        public void DadoQueAUrlDoEndPointEh(string endpoint) => _restRequest.Resource = endpoint;
+
+        [Given(@"que o método http é '(.*)'")]
+        public void DadoQueOMetodoHttpEh(string metodo)
+        {
+            if (metodo.ToUpper() == "GET")
+                _restRequest.Method = Method.GET;
+
+            if (metodo.ToUpper() == "POST")
+                _restRequest.Method = Method.POST;
+
+            if (metodo.ToUpper() == "PUT")
+                _restRequest.Method = Method.PUT;
+
+            if (metodo.ToUpper() == "DELETE")
+                _restRequest.Method = Method.DELETE;
+        }
+
+
+        [Given(@"que o id é (.*)")]
+        public void DadoQueOIdDoClienteEh(int id) => _id = id;
+
+        [Given(@"que o name é (.*)")]
+        public void DadoQueONomeDoClienteEh(string name) => _name = name;
+
+        [Given(@"que o cpf é (.*)")]
+        public void DadoQueOCPFDoClienteEh(string cpf) => _cpf = cpf;
+
+        [Given(@"que o hash é (.*)")]
+        public void DadoQueOHashDoClienteEh(string hash) => _hash = hash;
+
+        [When(@"obter o cliente")]
+        public void QuandoObterOCliente() => Client();
+
+
+        [Then(@"a resposta será (.*)")]
+        public void EntaoARespostaSera(HttpStatusCode statusCode) => Assert.Equal(statusCode, _restResponse.StatusCode);
+
+
+        public void Client()
+        {
+            _restRequest.AddHeader("Content-Type", "application/json");
+
+            if(_restRequest.Method == Method.GET)
+            {
+                if (_id != 0)
+                    _restRequest.AddParameter("id", _id);
+            }               
+            else if(_restRequest.Method == Method.POST)
+                _restRequest.AddJsonBody(new { Name = _name, CPF = _cpf, Hash = _hash });
+
+            _restClient.BaseUrl = new Uri(_host);
+            _restResponse = _restClient.Execute(_restRequest);
+        }
+    }
+}

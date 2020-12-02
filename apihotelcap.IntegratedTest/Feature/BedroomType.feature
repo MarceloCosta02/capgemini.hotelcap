a9a59d7d8a09257bb4a4657fdc5ad3ab8ff37cbd
Marcelo Ponte <marcelo.ponte@capgemini.com>
2020-12-02 10:00:20 -0300

    Criando testes integrados de BedroomType

---
+++ apihotelcap.IntegratedTest/Feature/BedroomType.feature
@@ -0,0 +1,24 @@
+﻿Funcionalidade: BedroomType
+Testes integrados das funcionalidade relacionadas ao end-point BedroomType
+
+
+Esquema do Cenario: Listar todos os tipos de quarto
+	Dado que o endpoint bedroomType é 'BedroomType'
+	E que o método http do bedroomType é 'GET'
+	Quando obter o tipo de quarto
+	Então a resposta de bedroomType será 200
+	
+Esquema do Cenario: Listar um tipo de quarto pelo Id
+	Dado que o endpoint bedroomType é 'BedroomType/getById'
+	E que o método http do bedroomType é 'GET'
+	E que o id do tipo de quarto é 1
+	Quando obter o tipo de quarto
+	Então a resposta de bedroomType será 200	
+
+Esquema do Cenario: Criar tipos de quarto
+	Dado que o endpoint bedroomType é 'BedroomType'
+	E que o método http do bedroomType é 'POST'
+	E que a descricao é "Presidencial"
+	E que o valor é 1200	
+	Quando obter o tipo de quarto
+	Então a resposta de bedroomType será 201	
---
+++ apihotelcap.IntegratedTest/Steps/BedroomTypeSteps.cs
@@ -0,0 +1,91 @@
+﻿using BoDi;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace apihotelcap.IntegratedTest.Steps
+{
+    [Binding]
+    public class BedroomTypeSteps
+    {
+        private string _host = "https://localhost:44314/api/";
+        private IRestClient _restClient;
+        private IRestRequest _restRequest;
+        private IRestResponse _restResponse;
+        private IObjectContainer _objectContainer;
+        private int _id;
+        private string _description;
+        private double _value;
+
+        public BedroomTypeSteps(IObjectContainer objectContainer) => _objectContainer = objectContainer;
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            _restClient = new RestClient();
+            _objectContainer.RegisterInstanceAs(_restClient);
+            _restRequest = new RestRequest();
+            _objectContainer.RegisterInstanceAs(_restRequest);
+            _restResponse = new RestResponse();
+            _objectContainer.RegisterInstanceAs(_restResponse);
+            _restClient.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
+        }        
+
+        [Given(@"que o endpoint bedroomType é '(.*)'")]
+        public void DadoQueAUrlDoEndPointEh(string endpoint) => _restRequest.Resource = endpoint;
+
+        [Given(@"que o método http do bedroomType é '(.*)'")]
+        public void DadoQueOMetodoHttpEh(string metodo)
+        {
+            if (metodo.ToUpper() == "GET")
+                _restRequest.Method = Method.GET;
+
+            if (metodo.ToUpper() == "POST")
+                _restRequest.Method = Method.POST;
+
+            if (metodo.ToUpper() == "PUT")
+                _restRequest.Method = Method.PUT;
+
+            if (metodo.ToUpper() == "DELETE")
+                _restRequest.Method = Method.DELETE;
+        }
+
+
+        [Given(@"que o id do tipo de quarto é (.*)")]
+        public void DadoQueOIdDoTipoDeQuartoEh(int id) => _id = id;
+
+        [Given(@"que a descricao é (.*)")]
+        public void DadoQueADescricaoEh(string description) => _description = description;
+
+        [Given(@"que o valor é (.*)")]
+        public void DadoQueOValorEh(double value) => _value = value;
+
+        [When(@"obter o tipo de quarto")]
+        public void QuandoObterOTipoDeQuarto() => BedroomType();
+
+
+        [Then(@"a resposta de bedroomType será (.*)")]
+        public void EntaoARespostaSera(HttpStatusCode statusCode) => Assert.Equal(statusCode, _restResponse.StatusCode);
+
+
+        public void BedroomType()
+        {
+            _restRequest.AddHeader("Content-Type", "application/json");
+
+            if(_restRequest.Method == Method.GET)
+            {
+                if (_id != 0)
+                    _restRequest.AddParameter("id", _id);
+            }               
+            else if(_restRequest.Method == Method.POST)
+                _restRequest.AddJsonBody(new { Description = _description, Value = _value });
+
+            _restClient.BaseUrl = new Uri(_host);
+            _restResponse = _restClient.Execute(_restRequest);
+        }
+    }
+}
